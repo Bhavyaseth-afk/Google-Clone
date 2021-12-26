b8b7a02bfff491bbeb75ccae00ba4bd0def7e15e
Bhavya seth <19bmc015@ietdavv.edu.in>
2021-12-26 22:07:33 +0530

    third

--- googleclone/settings.py
+++ googleclone/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--m3oyvd3l$wg0#nsx)b+g%=uk$3)1yhw7lb@gf!2+t-@*kptaf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
